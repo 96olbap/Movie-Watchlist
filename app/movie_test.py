ca777a8c0a94e3c39b0edd8ded9547a1157cf967
96olbap <alvinwanjala@gmail.com>
2022-04-27 12:45:50 +0300

    Add views.py file

--- app/movie_test.py
+++ app/movie_test.py
@@ -10,7 +10,7 @@ class MovieTest(unittest.TestCase):
         '''
         Set up method that will run before every test
         '''
-        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
+        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','khsjha27hbs',8.5,129993)
 
     def test_init(self):
         '''
