ff072f47f852757a731d6ce08d7b35f3220345aa
96olbap <alvinwanjala@gmail.com>
2022-04-28 12:01:40 +0300

    This commit will add the main subfolder

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,37 @@
 from flask import Flask
-from .config import DevConfig
 from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    #Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    #Initializing flask extensions
+    bootstrap.init_app(app)
+
+    #Registering a blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
+
+
+
 
 # Initializing application
-app = Flask(__name__, instance_relative_config = True)
+# app = Flask(__name__, instance_relative_config = True)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+# # Setting up configuration
+# app.config.from_object(DevConfig)
+# app.config.from_pyfile('config.py')
 
-#Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+# #Initializing Flask Extensions
 
-from app import views
-from app import error
+
+# from app import views
+# from app import error
